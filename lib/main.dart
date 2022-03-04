c3e04d4c0818b25b7fbac3141b1cfa9ef6a740ec
Amdebirhan <amdebirhanteshome@gmail.com>
2022-03-03 21:13:51 -0800

    one time onboard screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/db/db_healper.dart';
+import 'package:todo_app/onboard/onboard.dart';
 import 'package:todo_app/services/theme.services.dart';
 import 'package:todo_app/ui/theme.dart';
 import 'ui/Home_Page.dart';
 
+int? isviewed;
 Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+
   WidgetsFlutterBinding.ensureInitialized();
   await DBHelper.instance;
   await GetStorage.init();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  isviewed = prefs.getInt('onBoard');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -25,7 +40,7 @@ class MyApp extends StatelessWidget {
       theme: Themes.light,
       darkTheme: Themes.dark,
       themeMode: ThemeService().theme,
-      home: const HomePage(),
+      home: isviewed != 0 ? OnBoard() : HomePage(),
     );
   }
 }
